84ee032d2b4280ddbb3113470f77c9b2bea6167b
Dev. Ebisa <ebisatesfaye30@gmail.com>
2025-03-24 11:22:38 +0300

    baseURL

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const axiosinstance = axios.create({
     //local backend 
     // baseURL : "http://127.0.0.1:5001/clone-b74d3/us-central1/api"
-    baseURL : "http://localhost:5000"
+    // baseURL : "http://localhost:5000"
     //backend without firebase functions 
-    // baseURL : "https://amazon-backend-api-s3ym.onrender.com",
+    baseURL : "https://amazon-backend-api-s3ym.onrender.com",
 });
 
 export {axiosinstance}
